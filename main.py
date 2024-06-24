7d10d5fcd94e79e5f59de118a3cf0d8ff192f62c
Jeremiah Jeschke <mail@officeautomata.com>
2024-06-24 04:08:13 -0600

    Edits

--- main.py
+++ main.py
@@ -36,9 +36,9 @@ st.markdown("""<p align="center">
 
 st.markdown("---")
 
-st.markdown("""<p align="center">
+st.markdown("""<p align="center" markdown="1">
 ### Join our Private Beta!
-# </p>
+</p>
 <p align="center">
 <a href="https://definitive-ai.streamlit.app/Signup"><img src="https://raw.githubusercontent.com/Definitive-AI-Testing/agents-streamlit/master/.streamlit/sign-up.jpg" height="50" /></a>
 </p>""", unsafe_allow_html=True)
